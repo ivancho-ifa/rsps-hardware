c6989bc4821bd281e70482cb0747196a2d3df538
Ivan Mollov <ivan.xero.mollov@gmail.com>
2020-08-19 14:45:30 +0300

    Connected to display, Clang-Format and VS Code setup

--- RSPS-hardware.ino
+++ RSPS-hardware.ino
@@ -8,21 +8,32 @@
  *   Done:
  *     @todo Implement configuration mode
  *     @todo Collect data from GPS sensor
- *     @todo Display data in website
+ *     @todo Print data on display
  *
  *   To be done:
- *     @todo Print data on display
  *     @todo Send data to an MQTT broker
  *     @todo Parse data from MQTT broker (server side)
+ *     @todo Display data in website
  *     @todo Speed meter with a magnet detector
  */
 
 
+// ESP8266 libraries
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
 
+// GPS libraries
 #include <SoftwareSerial.h>
 #include <TinyGPS++.h>
+
+// OLED display libraries
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+
+#define SCREEN_WIDTH 128 ///< OLED display width, in pixels
+#define SCREEN_HEIGHT 64 ///< OLED display height, in pixels
 
 
 using on_parsed_gps = void (*)();
@@ -34,6 +45,7 @@ const int8_t gps_tx{D4}; ///< The pin connected to the RX pin of the GPS receive
 const uint32_t gps_baud{9600}; ///< The default baud rate of the GPS receiver.
 SoftwareSerial gps_serial{gps_rx, gps_tx};
 
+String own_ssid;
 const String own_ssid_prefix{"rsps-tracker-"}; ///< This device's network SSID prefix when in soft AP mode.
 String id{"000"}; ///< This device's ID.
 ESP8266WebServer configuration_server; ///< The configuration configuration_server of this device.
@@ -41,10 +53,23 @@ ESP8266WebServer configuration_server; ///< The configuration configuration_serv
 const uint8_t button_0{D0}; ///< Button "0" of the device.
 bool is_in_configuration_mode{false}; ///< The state of button "0".
 
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1); ///< Display connected to the I2C pins
+
 
 void setup() {
 	Serial.begin(115200);
 	Serial.println();
+
+	display.begin(SSD1306_SWITCHCAPVCC, 0x3C); ///< I2C address 0x3C. Change if collision with another sensor
+	delay(2000); ///< Leaving time for the display to initialize
+	display.clearDisplay();
+
+	display.setTextSize(1);
+	display.setTextColor(WHITE);
+
+	println("Ride safe, park safe!", 0, 0);
+	display.display();
+	delay(5000);
 
 	gps_serial.begin(gps_baud);
 
@@ -61,8 +86,31 @@ void loop() {
 	gps_parse(display_info);
 
 	/// @todo Test how this is behaving on disconnecting the GPS receiver...
-	if (millis() > 5000 && gps.charsProcessed() < 10)
-		Serial.println("No GPS data received... check wiring or find a place with better signal");
+	if (millis() > 5000 && gps.charsProcessed() < 10) {
+		display.clearDisplay();
+		println("No GPS data received... check wiring or find a place with better signal", 0, 0);
+		display.display();
+	}
+}
+
+
+void print(const char* message) {
+	Serial.print(message);
+	display.print(message);
+}
+
+void println(const char* message) {
+	Serial.println(message);
+	display.println(message);
+}
+
+void println(const char* message, int16_t x, int16_t y) {
+	display.setCursor(x, y);
+	print(message);
+}
+
+void println(const String& message) {
+	println(message.c_str());
 }
 
 
@@ -98,16 +146,14 @@ void gps_parse(const on_parsed_gps& cb) {
  */
 
 void configuration_mode() {
-	const String own_ssid{own_ssid_prefix + id};
+	own_ssid = own_ssid_prefix + id;
 	if (WiFi.softAP(own_ssid, "")) {
-		Serial.println("Entered \"Client setup mode\"...");
+		display.clearDisplay();
+		println("Entered \"Client setup mode\"...", 0, 0);
+		display.display();
+		delay(5000); ///< Leave time for the user to read the message
 
-		const IPAddress own_ip{WiFi.softAPIP()};
-		/// @todo Display this on the device screen
-		Serial.println("To configure the tracker:");
-		Serial.printf("1. Connect to Wi-Fi network %s (no password)\n", own_ssid.c_str());
-		Serial.print("2. Connect to IP ");
-		Serial.println(own_ip);
+		print_configuration_mode_info();
 
 		configuration_server.on("/", []() {
 			const char* page =
@@ -137,23 +183,24 @@ void configuration_mode() {
 						&& plain.lastIndexOf('=') == tracker_id_prefix.length() - 1) {
 						id = plain.substring(tracker_id_prefix.length());
 
-						const String confirmationMessage = "Successfully set tracker's ID to " + id;
-						Serial.println(confirmationMessage);
-						configuration_server.send(200, "text/plain", confirmationMessage);
+						const String confirmation_message = "Successfully set tracker's ID to " + id;
+						print_message_in_configuration_mode(confirmation_message);
+
+						configuration_server.send(200, "text/plain", confirmation_message);
 
 						return; ///< Stop execution on success
 					}
 				}
 
 				/// Handle parsing error
-				const String errorMessage = "Failed to parse tracker-id field";
-				Serial.println(errorMessage);
-				configuration_server.send(400, "text/plain", errorMessage);
+				const char* error_message = "Failed to parse tracker-id field";
+				print_message_in_configuration_mode(error_message);
+				configuration_server.send(400, "text/plain", error_message);
 			}
 			else {
 				/// @todo Print name of method.
 				const char* errorMessage = "Method Not Allowed";
-				Serial.println(errorMessage);
+				print_message_in_configuration_mode(errorMessage);
 				configuration_server.send(405, "text/plain", errorMessage);
 			}
 		});
@@ -170,75 +217,98 @@ void configuration_mode() {
 				is_in_configuration_mode = !is_in_configuration_mode;
 		}
 
-		Serial.println("Exiting \"Client setup mode\"...");
+		display.clearDisplay();
+		println("Exiting \"Client setup mode\"...", 0, 0);
+		display.display();
+		delay(5000);
+
 		configuration_server.stop();
 	}
 }
 
 
+void print_configuration_mode_info() {
+	display.clearDisplay();
+	display.setCursor(0, 0);
+	println("To configure the tracker:");
+	println("- Connect to Wi-Fi network "+ own_ssid + " (no password)");
+	println("- Connect to IP " + WiFi.softAPIP().toString());
+	display.display();
+}
+
+void print_message_in_configuration_mode(const char* message) {
+	display.clearDisplay();
+	println(message, 0, 0);
+	display.display();
+	delay(5000);
+	print_configuration_mode_info();
+}
+
+void print_message_in_configuration_mode(const String& message) {
+	print_message_in_configuration_mode(message.c_str());
+}
+
+
 void display_info() {
+	display.clearDisplay();
+	display.setCursor(0,0);
+
 	if (gps.location.isValid()) {
-		Serial.print("Latitude: ");
-		Serial.println(gps.location.lat(), 6);
-		Serial.print("Longitude: ");
-		Serial.println(gps.location.lng(), 6);
+		println("Latitude: " + String(gps.location.lat(), 6));
+		println("Longitude: " + String(gps.location.lng(), 6));
 
-		if (gps.altitude.isValid()) {
-			Serial.print("Altitude: ");
-			Serial.println(gps.altitude.meters());
-		}
+		if (gps.altitude.isValid())
+			println("Altitude: " + String(gps.altitude.meters()));
 		else
-			Serial.println("Altitude: Not Available");
+			println("Altitude: Not Available");
 	}
 	else
-		Serial.println("Location: Not Available");
+		println("Location: Not Available");
 
-	Serial.print("Date: ");
-	if (gps.date.isValid()) {
-		Serial.print(gps.date.month());
-		Serial.print("/");
-		Serial.print(gps.date.day());
-		Serial.print("/");
-		Serial.println(gps.date.year());
-	}
+	print("Date: ");
+	if (gps.date.isValid())
+		println(String{} + gps.date.month() + "/" + gps.date.day() + "/" + gps.date.year());
 	else
-		Serial.println("Not Available");
+		println("Not Available");
 
-	Serial.print("Time: ");
-	// Display time in HH:MM:SS.CC
+	print("Time: ");
+	/// Display time in HH:MM:SS.CC
 	if (gps.time.isValid()) {
-		if (gps.time.hour() < 10)
-			Serial.print(F("0"));
-		Serial.print(gps.time.hour());
+		String time_format;
 
-		Serial.print(":");
+		if (gps.time.hour() < 10)
+			time_format += "0";
+		time_format += gps.time.hour();
+
+		time_format += ":";
 
 		if (gps.time.minute() < 10)
-			Serial.print(F("0"));
-		Serial.print(gps.time.minute());
+			time_format += "0";
+		time_format += gps.time.minute();
 
-		Serial.print(":");
+		time_format += ":";
 
 		if (gps.time.second() < 10)
-			Serial.print(F("0"));
-		Serial.print(gps.time.second());
+			time_format += "0";
+		time_format += gps.time.second();
 
-		Serial.print(".");
+		time_format += ".";
 
 		if (gps.time.centisecond() < 10)
-			Serial.print(F("0"));
-		Serial.println(gps.time.centisecond());
+			time_format += "0";
+		time_format += gps.time.centisecond();
+
+		println(time_format);
 	}
 	else
-		Serial.println("Not Available");
+		println("Not Available");
 
-	Serial.print("Speed: ");
+	print("Speed: ");
 	if (gps.speed.isValid())
-		Serial.println(gps.speed.kmph());
+		println(String(gps.speed.kmph(), 6));
 	else
-		Serial.println("Not Available");
+		println("Not Available");
 
 	Serial.println();
-	Serial.println();
-	delay(1000);
+	display.display();
 }
